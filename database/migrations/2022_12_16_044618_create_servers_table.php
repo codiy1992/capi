b80dadfa6bfcefb90fed042adc2bf8790c5e5cd5
codiy <mail@codiy.net>
2022-12-16 16:10:31 +0800

    fix

--- database/migrations/2022_12_16_044618_create_servers_table.php
+++ database/migrations/2022_12_16_044618_create_servers_table.php
@@ -19,6 +19,7 @@ class CreateServersTable extends Migration
             $table->string('name', 32)->default('')->comment('节点名称');
             $table->string('ipv4', 32)->default('')->comment('节点地址');
             $table->tinyInteger('status')->default(1)->comment('状态 0:未启用 1:已启用');
+            $table->tinyInteger('debug')->default(0)->comment('协议调试');
             $table->unique('ipv4');
         });
     }
