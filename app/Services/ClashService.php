5261ba1c410b91f79b1acaed54b1e7d9d4c25d94
codiy <mail@codiy.net>
2022-12-21 17:38:49 +0800

    fix

--- app/Services/ClashService.php
+++ app/Services/ClashService.php
@@ -154,7 +154,7 @@ class ClashService
                 $proxy['name'] = sprintf("%s.%s.%s.%s", $server->group, $server->name, $protocol->name, $protocol->transport);
                 $proxy['type'] = $protocol->name;
                 $proxy['server'] = $protocol->tls ? sprintf('%s.0x256.com', $server->ipv4) : $server->ipv4;
-                if (!empty($proxy['plugin']) && $in_array(proxy['plugin'], ['v2ray-plugin'])) {
+                if (!empty($proxy['plugin']) && in_array(proxy['plugin'], ['v2ray-plugin'])) {
                     $proxy['plugin-opts']['host'] = $proxy['server'];
                 }
                 $proxies[] = $proxy;
